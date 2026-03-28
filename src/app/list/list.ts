0db44d5c41eedcac50f91a177cc177b981c22b59
Kévin Lionnet <24947582+Cemus@users.noreply.github.com>
2026-03-28 15:28:25 +0100

    fix: list component fetching data

--- src/app/list/list.ts
+++ src/app/list/list.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api-service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './list.html',
   styleUrl: './list.css',
 })
-export class List {}
+export class List implements OnInit {
+  items$!: Observable<any>;
+  constructor(private readonly apiService: ApiService) {
+    this.items$ = this.apiService.items$;
+  }
+
+  ngOnInit(): void {
+    console.log(this.apiService.getItems());
+  }
+}
