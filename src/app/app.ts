5ab678c0bfe04c5284d46082853beff0ba77ae20
Kévin Lionnet <24947582+Cemus@users.noreply.github.com>
2026-03-28 15:10:46 +0100

    feat: add main components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Detail } from './detail/detail';
+import { History } from './history/history';
+import { List } from './list/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Detail, History, List],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('ct-takehome');
