b4c6bc93bd1b3ce31c2d3bdd8a761f33babec817
Kévin Lionnet <24947582+Cemus@users.noreply.github.com>
2026-03-28 15:53:58 +0100

    feat(history): implement logic to display title history

--- src/app/history/history.ts
+++ src/app/history/history.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { ApiService } from '../services/api-service';
+import { Item } from '../models/item.model';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './history.html',
   styleUrl: './history.css',
 })
-export class History {}
+export class History implements OnChanges {
+  @Input() selectedItemId: number | null = null;
+
+  item$!: Observable<Item | null>;
+
+  constructor(private readonly apiService: ApiService) {}
+
+  ngOnChanges() {
+    this.item$ = this.apiService.items$.pipe(
+      map((items: Item[]) => items.find((i) => i['id'] === this.selectedItemId) ?? null),
+    );
+  }
+}
